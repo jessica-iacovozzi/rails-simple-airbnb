e656f7a3bc698e365e5507b381ae4d9f9ac63c23
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-09 16:53:36 -0500

    functionning app, missing styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Clearing All Flats...'
+Flat.destroy_all
+puts 'Clean Database'
+puts 'Creating 5 flats...'
+
+5.times do
+  flat = Flat.create!(
+    name: Faker::App.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(70..300),
+    number_of_guests: rand(1..12),
+    rating: rand(1..5)
+  )
+  puts "Flat number #{flat.id} created!"
+end
+
+puts 'All done!'
